29d143ed5c38baffaddcea3ff8dc36a8c5c90a15
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-05-09 09:41:18 -0400

    styling added

--- src/scripts/face.js
+++ src/scripts/face.js
@@ -1,9 +1,12 @@
+import Platform from "./platform";
+
 const CONSTANT = {
-    gravity: 0.4
+    gravity: 0.5
 }
 
 class Face {
   constructor(canvas) {
+    this.canvas = canvas
     this.dimensions = {
       x: 100,
       y: 300, 
@@ -11,15 +14,14 @@ class Face {
 
     this.velocity = {
       x: 0,
-      y: 2,
+      y: 1,
     };
-    this.canvas = canvas
 
     this.width = 50;
     this.height = 50;
 
-    this.xspeed = 5;
-    this.yspeed = 5;
+    this.xspeed = 10;
+    this.yspeed =  9;
     this.friction = 0.6;
     this.maxSpeed = 10;
 
@@ -41,8 +43,6 @@ class Face {
       this.height
     );
   }
-
-  
 
   setupInputs() {
     document.addEventListener("keydown", (event) => {
@@ -66,35 +66,76 @@ class Face {
     });
   }
 
-  step() {
+//   step() {
+//     if (this.active) {
+//       //Horizantal
+
+//       if (this.rightKey) {
+
+//         //Slow Down
+//         this.velocity.x = this.xspeed;
+//       } else if (this.leftKey) {
+//         this.velocity.x = -this.xspeed;
+//       }
+//        else if (this.spaceKey) {
+//         this.velocity.y = -this.yspeed;
+//       } else if(this.dimensions.y + this.height + this.velocity.y < this.canvas.height) {
+//         // this.velocity.y = 1
+//         this.velocity.y += CONSTANT.gravity
+       
+//       }
+//       else {
+//         this.velocity.x = 0;
+
+//         this.velocity.y = 0;
+//       }
+      
+
+//       //Vertical
+
+//       this.dimensions.x += this.velocity.x;
+//       this.dimensions.y += this.velocity.y;
+//     }
+//   }
+
+// ccccptttt 
+
+step() {
     if (this.active) {
       //Horizantal
-
       if (this.rightKey) {
-
         //Slow Down
         this.velocity.x = this.xspeed;
       } else if (this.leftKey) {
         this.velocity.x = -this.xspeed;
-      }
-       else if (this.spaceKey) {
-        this.velocity.y = -this.yspeed;
-      } else if(this.dimensions.y + this.height + this.velocity.y < this.canvas.height) {
-        // this.velocity.y = 1
-        this.velocity.y += CONSTANT.gravity
-      }
-      else {
+      } else {
         this.velocity.x = 0;
-
+      }
+      
+      //Vertical
+      if (this.spaceKey && this.dimensions.y > 0) {
+        this.velocity.y = -this.yspeed;
+      } else if (this.dimensions.y + this.height + this.velocity.y < this.canvas.height) {
+        this.velocity.y += CONSTANT.gravity;
+      } else {
         this.velocity.y = 0;
       }
-
-      //Vertical
-
+      
+      // Update position
       this.dimensions.x += this.velocity.x;
       this.dimensions.y += this.velocity.y;
+      
+      // Keep face inside canvas
+      if (this.dimensions.x < 0) {
+        this.dimensions.x = 0;
+      } else if (this.dimensions.x + this.width > this.canvas.width) {
+        this.dimensions.x = this.canvas.width - this.width;
+      }
     }
   }
+  
+
+
 // jump(){
 //     if (this.) {
 //      this.velocity.y = -this.yspeed;
--- src/scripts/game.js
+++ src/scripts/game.js
@@ -7,25 +7,27 @@ class Game {
     constructor(canvasEl){
         this.canvas = canvasEl
         console.log("ol")
-        this.canvas.width = 500;
+        this.canvas.width = 800;
         this.canvas.height = 400;
+        // this.canvas.setAttribute('style', "position: absolute;  left: 50%;margin-left:-400px; top: 50%;margin-top:-300px; border:2px solid blue")
         this.face = new Face(this.canvas)
 
         // this.level = new Level(0,350)
         this.ctx = this.canvas.getContext("2d")
+        // this.background = new Image ()
+        // this.background.src = "src/images/5246723.jpg"
+        // this.background.width = 1024
+        // this.background.height = 1000
 
-      this.levelFloor = [
-            new Level (0,366),
-            new Level (50,366),
-            new Level (100,366),
-            new Level (150,366),
-            new Level (200,366),
-            new Level (250,366),
-            new Level (300,366),
-            new Level (350,366),
-            new Level (400,366),
-            new Level (450,366)
-        ]
+      this.levelFloor = []
+        // Create bottom platform
+        let x = 0
+            for(let i = 0; i< 16; i++){
+                let y = 366
+                this.levelFloor.push(new Level (x, y))
+                 x+= 50
+            }
+        
 
         this.platforms = [
             new Platform (120,230),
@@ -37,28 +39,28 @@ class Game {
             new Platform (400,270),
         ]
        
-       
         this.face.setupInputs()
 
      
 
-        this.powerUps =[
-            new PowerUps (250,335),
+        this.powers =[
+            new PowerUps (200,335),
             new PowerUps (160,205)
         ]
-        for(let i = 0; i < this.powerUps.length; i++){
-            this.powerUps[i].draw(this.ctx)
-        }
+        
        
     }
 
   animate(){
     this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height)
-    this.drawBackground()
+    // this.drawBackground()
+    this.drawCanvas()
     this.drawLevel()
     this.drawPlatform()
     this.face.draw(this.ctx)
     this.face.step()
+    this.drawPower()
+    
 
     requestAnimationFrame(this.animate.bind(this))
   }
@@ -75,9 +77,13 @@ gameover(){
 
 }
 
-drawBackground(){
-    this.ctx.fillStyle = "beige";
-    this.ctx.fillRect(0, 0, 500, 400);
+// drawBackground(){
+//     this.ctx.drawImage(this.background, 0, 0, this.background.width, this.background.height)
+// }
+
+drawCanvas(){
+    this.ctx.fillStyle = "#B5D5F1"
+    this.ctx.fillRect(0, 10, 800, 600);
 }
 
 drawLevel(){
@@ -94,6 +100,22 @@ drawPlatform(){
         this.platforms[i].draw(this.ctx)
     }
 }
+
+drawPower(){
+    for(let i = 0; i < this.powers.length; i++){
+        this.powers[i].draw(this.ctx)
+    }
+}
+
+stand(){
+    for(let i = 0; i < this.platforms.length; i++){
+   if(this.face.dimensions.y + this.face.height <= this.platforms[i].dimensions.y) {
+    this.face.velocity.y = 0
+
+   }
+}
+}
+
 
 
 }
